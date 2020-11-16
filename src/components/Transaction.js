63f174f326731af4fb620f3afce6abfd56262b62
Abhilash <abhilashmalhotra17@gmail.com>
2020-11-16 20:03:42 +0530

    UI completed

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState'
 
-export const Transaction = ({ text, amount }) => {
+export const Transaction = ({ id, text, amount }) => {
+    const { deleteTransaction } = useContext(GlobalContext)
     const sign = amount < 0 ? '-' : '+';
     return (
         <li className={`${amount < 0 ? 'minus' : 'plus'}`}>
-            {text} <span>{sign} ${Math.abs(amount)}</span><button className="delete-btn">x</button>
+            {text} <span>{sign} ${Math.abs(amount)}</span><button className="delete-btn" onClick={() => deleteTransaction(id)}>x</button>
         </li>
     )
 }
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,16 @@
+// eslint-disable-next-line
 export default (state, action) => {
     switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transations: state.transations.filter(transation => transation.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transations: [action.payload, ...state.transations]
+            }
         default:
             return state
     }
